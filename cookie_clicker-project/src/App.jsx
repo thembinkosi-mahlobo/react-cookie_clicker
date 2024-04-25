42e294d715aebe9aadb329f0a4f283313f5d88d0
Thembinkosi Mahlobo <themba.romano@hotmail.co.uk>
2024-04-25 16:19:31 +0100

    add updates and changes made to  App.jsx, index.css, MyComponent and MyImages

--- cookie_clicker-project/src/App.jsx
+++ cookie_clicker-project/src/App.jsx
@@ -1,27 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import MyComponent from "./MyComponent";
 
-import "./App.css";
+export default function App() {
+  const [cookies, setCookies] = useState(0);
+  const [cps, setCps] = useState(0);
 
-function App() {
-  const [count, setCount] = useState(0);
+  function addCookies() {
+    setCookies((currentCookies) => {
+      return currentCookies + 1;
+    });
+  }
 
+  function buyUpgrade() {
+    if (cookies >= 20) {
+      setCps(cps + 4);
+      setCookies(-20);
+    } else {
+      alert("You need more cookies to by Upgrade");
+    }
+  }
+
+  function earnReward() {
+    if (cookies >= 200) {
+      setCps(cps + 10);
+      setCookies(-200);
+    } else {
+      alert("You need more cookies to by Upgrade");
+    }
+
+    useEffect(() => {
+      const interval = setInterval(()=>{
+        setCookies((currentCookies)=>currentCookies + cps), 1000/cps);
+      return ()=>{
+        clearInterval(interval);
+      };
+    },[cps]);
+  }
+
+  
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="App">
+        <img src="./src/MyImages/mario backgroung.png" alt="Click the Cookie" />
+        <h1>Cookie Clicker</h1>
+        <p>Cookies {cookies}</p>
+        <p>Cookies per Second {cps}</p>
+        <button onClick={addCookies}>click me</button>
+        <button onClick={buyUpgrade}>Upgrade 20 cookies for 4cps</button>
+        <button onClick={earnReward}>Reward 200 cookies for 10cps</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="board"></div>
+      <MyComponent count={cookies} />
     </>
   );
 }
-
-export default App;
